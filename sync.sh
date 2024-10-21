5047933774738d87a999529108491c3c47ff1f9f
Meir Michanie <meirm@riunx.com>
2024-10-21 09:28:10 +0200

    sync to prod with confirmation

--- sync.sh
+++ sync.sh
@@ -1,1 +1,8 @@
-rsync -av  --exclude postges_data --exclude .git --exclude __pycache__/ --exclude migrations/ --exclude .env . meirm@tts:/opt/django_project/.
+#!/bin/bash
+rsync -av --dry-run --size-only  --include  pages/migrations/__init__.py  --exclude postges_data --exclude .git --exclude __pycache__/ --exclude 'migrations/*_*.*' . meirm@tts:/opt/django_project/.
+
+echo "Do you want to sync the files? (y/N)"
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    rsync -av --size-only  --include  pages/migrations/__init__.py  --exclude postges_data --exclude .git --exclude __pycache__/ --exclude 'migrations/*_*.*' . meirm@tts:/opt/django_project/.
+fi
