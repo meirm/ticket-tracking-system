f8eed5c9bdc27ca3ff3be4d87f6aa92987485ea0
Meir Michanie <meirm@cyborg.fi>
2025-01-27 18:20:49 -0600

    Enhance sample API request script with command-line options and default values

--- sample_api_request.sh
+++ sample_api_request.sh
@@ -1,18 +1,68 @@
-#!/bin/bash -v
+#!/bin/bash
 WEBHOOK_URL="http://localhost:9000/tickets/api/v1/create/"
-API_KEY="2b8263c9fa42c597bfe364288e59ace872bc8bd08a8b429dc393ce30f5b009bc"
-TITLE_TEMPLATE="New ticket created"
-BODY_TEMPLATE="A new ticket has been created by the user"
-PRIORITY="High"
 
+
+if [ -z "$API_KEY" ]; then
+    echo "API_KEY is not set"
+    exit 1
+fi
+
+while getopts "t:d:p:c:g:a:s:he" opt; do
+case ${opt} in
+    t )
+      title=$OPTARG
+      ;;
+    d )
+      description=$OPTARG
+      ;;
+    p )
+      priority=$OPTARG
+      ;;
+    c )
+      category=$OPTARG
+      ;;
+    g )
+      assigned_group=$OPTARG
+      ;;
+    a )
+      assignee=$OPTARG
+      ;;
+    s)
+      status=$OPTARG
+      ;;
+    h )
+      echo "Usage: cmd [-t title] [-d description] [-p priority] [-c category] [-g assigned_group] [-a assignee] [-s status]" 1>&2
+      echo "Default values: priority=High, category=Support, assigned_group=staff, assignee=meirm" 1>&2
+      exit 0
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      exit 1
+      ;;
+    * )
+      echo "Invalid option: $OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+
+
+
+# set default values
+TITLE_TEMPLATE=${title:-"New ticket created"}
+BODY_TEMPLATE=${description:-"A new ticket has been created by the user"}
+PRIORITY=${priority:-"High"}
+CATEGORY=${category:-"Support"}
+ASSIGNED_GROUP=${assigned_group:-"staff"}
+ASSIGNEE=${assignee:-"meirm"}
 # Format the JSON payload
 PAYLOAD=$(jq -n \
   --arg title "$TITLE_TEMPLATE" \
   --arg description "$BODY_TEMPLATE" \
   --arg priority "$PRIORITY" \
-  --arg category "Support" \
-  --arg assigned_group "staff" \
-  --arg assignee "meirm" \
+  --arg assigned_group "$ASSIGNED_GROUP" \
+  --arg category "$CATEGORY" \
+  --arg assignee "$ASSIGNEE" \
   '{title: $title, description: $description, priority: $priority, category: $category, assigned_group: $assigned_group, assignee: $assignee, status: "Open"}')
 
 echo $PAYLOAD
